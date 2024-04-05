af0322d50490be7dd7d054f5fcb651c5940a32fd
Didier Barriere Doleac <didierdoleac@hotmail.com>
2024-04-05 12:31:13 +0200

    Mise à jour des fichiers de configuration pour prendre en compte les changements de version d'Angular et de TypeScript

--- front/src/app/guards/auth.guard.ts
+++ front/src/app/guards/auth.guard.ts
@@ -1,9 +1,9 @@
 import {Injectable} from "@angular/core";
-import {CanActivate, Router} from "@angular/router"; 
+import { Router } from "@angular/router"; 
 import { SessionService } from "../services/session.service";
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor( 
     private router: Router,
--- front/src/app/guards/unauth.guard.ts
+++ front/src/app/guards/unauth.guard.ts
@@ -1,9 +1,9 @@
 import {Injectable} from "@angular/core";
-import {CanActivate, Router} from "@angular/router"; 
+import { Router } from "@angular/router"; 
 import { SessionService } from "../services/session.service";
 
 @Injectable({providedIn: 'root'})
-export class UnauthGuard implements CanActivate {
+export class UnauthGuard  {
 
   constructor( 
     private router: Router,
